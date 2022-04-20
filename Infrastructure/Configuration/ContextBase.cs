f7bc2d8f82de682080656fd411cabfda7cead382
guilhermetwelve0 <guilherme.cabral999@hotmail.com>
2022-04-20 15:10:15 -0300

    ATUALIZAÇÕES

--- Infrastructure/Configuration/ContextBase.cs
+++ Infrastructure/Configuration/ContextBase.cs
@@ -8,7 +8,7 @@ using System.Text;
 
 namespace Infrastructure.Configuration
 {
-    public class ContextBase : IdentityDbContext<ApplicationUser>
+    public class ContextBase : IdentityDbContext<IdentityUser>
     {
         public ContextBase(DbContextOptions<ContextBase> options) : base(options)
         {
