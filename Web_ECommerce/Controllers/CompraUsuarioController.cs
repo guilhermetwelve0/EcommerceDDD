993c553c1bbc8d1270e6477560796ecd5679b59d
guilhermetwelve0 <guilherme.cabral999@hotmail.com>
2022-05-02 14:49:04 -0300

    alt

---
+++ Web_ECommerce/Controllers/CompraUsuarioController.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web_ECommerce.Controllers
+{
+    public class CompraUsuarioController : Controller
+    {
+        public readonly UserManager<ApplicationUser> _userManager;
+
+        public CompraUsuarioController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        
+
+    }
+}
