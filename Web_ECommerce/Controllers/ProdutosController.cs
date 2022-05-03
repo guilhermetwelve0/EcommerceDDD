799dcf046227e6ccbebf1a9c4c9a5e43d40d123f
guilhermetwelve0 <guilherme.cabral999@hotmail.com>
2022-05-03 17:11:39 -0300

    alt

--- Web_ECommerce/Controllers/ProdutosController.cs
+++ Web_ECommerce/Controllers/ProdutosController.cs
@@ -20,10 +20,14 @@ namespace Web_ECommerce.Controllers
 
         public readonly InterfaceProductApp _InterfaceProductApp;
 
-        public ProdutosController(InterfaceProductApp InterfaceProductApp, UserManager<ApplicationUser> userManager)
+        public readonly InterfaceCompraUsuarioApp _InterfaceCompraUsuarioApp;
+
+        public ProdutosController(InterfaceProductApp InterfaceProductApp, UserManager<ApplicationUser> userManager, InterfaceCompraUsuarioApp InterfaceCompraUsuarioApp)
         {
             _InterfaceProductApp = InterfaceProductApp;
             _userManager = userManager;
+
+            _InterfaceCompraUsuarioApp = InterfaceCompraUsuarioApp;
         }
 
         // GET: ProdutosController
@@ -154,7 +158,36 @@ namespace Web_ECommerce.Controllers
             return Json(await _InterfaceProductApp.ListarProdutosComEstoque());
         }
 
+        public async Task<IActionResult> ListarProdutosCarrinhoUsuario()
+        {
+            var idUsuario = await RetornarIdUsuarioLogado();
+            return View(await _InterfaceProductApp.ListarProdutosCarrinhoUsuario(idUsuario));
+        }
 
+        // GET: ProdutosController/Delete/5
+        public async Task<IActionResult> RemoverCarrinho(int id)
+        {
+            return View(await _InterfaceProductApp.ObterProdutoCarrinho(id));
+        }
+
+        // POST: ProdutosController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> RemoverCarrinho(int id, Produto produto)
+        {
+            try
+            {
+                var produtoDeletar = await _InterfaceCompraUsuarioApp.GetEntityById(id);
+
+                await _InterfaceCompraUsuarioApp.Delete(produtoDeletar);
+
+                return RedirectToAction(nameof(ListarProdutosCarrinhoUsuario));
+            }
+            catch
+            {
+                return View();
+            }
+        }
 
 
     }
